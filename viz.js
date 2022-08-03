a813d414b1d531ee8b6551b27f96af50432f83c9
Amirah Adam <1901869@sit.singaporetech.edu.sg>
2022-08-03 18:06:55 +0800

    add slider

--- viz.js
+++ viz.js
@@ -12,12 +12,148 @@ function exportBar() {
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
-
+    
     // Parse the Data
     d3.csv("export.csv", function (data) {
 
-        console.log(data)
+        // console.log(data)
 
+        //Data preparation - able to get date
+        let exportData = data.map((x) => ({Commodity: x.Commodity, value: x.value, country: x.country, year: x.year}));
+        let listOfExport = [];
+        
+        // slider
+        var valuesSlider = document.getElementById('slider');
+        // shown on slider
+        var valuesForSlider = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];  
+        
+        // to get the year value
+        var format = {
+            to: function(value) {
+                return valuesForSlider[Math.round(value)];
+            },
+            from: function (value) {
+                return valuesForSlider.indexOf(Number(value));
+            }
+        };
+
+        // create slider
+        noUiSlider.create(valuesSlider, {
+            start: [2010],
+            range: { min: 0, max: valuesForSlider.length - 1 },
+            step: 1,
+            tooltips: true,
+            format: format,
+            pips: { mode: 'steps', format: format },
+        }).on('slide', function(e) { //when slide will change
+            console.log(e)
+
+            if(e == 2010){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2011){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2012){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2013){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2014){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2015){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2016){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2017){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+            else if(e == 2018){ // if slider = year
+                for(var i = 0; i < exportData.length; i++){
+                    if(e==exportData[i].year)
+                    {
+                        listOfExport.push(exportData[i])
+                    }
+                }
+            }
+
+            //Bars
+            svg.selectAll("exportBar")
+            .data(topTen(metrics)) // need change here
+            .enter()
+            .append("rect")
+            .attr("x", x_export(0))
+            .attr("y", function (d) { return y_export(d.key); })
+            .attr("width", function (d) { return x_export(d.value.total); }) 
+            .attr("height", y_export.bandwidth())
+            .attr("fill", "#69b3a2")
+
+            .on("mouseover", function (d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(d.key + "<br/> <br/> Total value : <b>" + d.value.total + "</b>")
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+        
+        });
+        
+        // the year range
+        valuesSlider.noUiSlider.set(['2010', '2018']);
+
+
+        // somehow need pass year here
+        // calcuate
         var metrics = d3.nest()
             .key(function (d) { return d.Commodity; })
             .rollup(function (v) {
@@ -26,9 +162,13 @@ function exportBar() {
                     total: d3.sum(v, function (d) { return d.value; })
                 };
             })
-            .entries(data);
-        console.log(metrics);
+            .entries(exportData);
+        
+        console.log(listOfExport);
+        console.log(exportData);
+        // console.log(metrics);
 
+        // get the top 10
         function topTen(data) {  //sorting to top 10 function
             data.sort(function (a, b) {
                 return b.value.total - a.value.total;
@@ -61,32 +201,7 @@ function exportBar() {
             .domain(topTen(metrics).map(function (d) { return d.key; }))
             .padding(.1);
         svg.append("g")
-            .call(d3.axisLeft(y_export))
-
-        //Bars
-        svg.selectAll("exportBar")
-            .data(topTen(metrics))
-            .enter()
-            .append("rect")
-            .attr("x", x_export(0))
-            .attr("y", function (d) { return y_export(d.key); })
-            .attr("width", function (d) { return x_export(d.value.total); })
-            .attr("height", y_export.bandwidth())
-            .attr("fill", "#69b3a2")
-
-            .on("mouseover", function (d) {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                div.html(d.key + "<br/> <br/> Total value : <b>" + d.value.total + "</b>")
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-            .on("mouseout", function (d) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            });
+            .call(d3.axisLeft(y_export)) 
     })
 
 }
@@ -112,7 +227,7 @@ function importBar() {
     // Parse the Data
     d3.csv("import.csv", function (data) {
 
-        console.log(data)
+        // console.log(data)
 
         var metrics = d3.nest()
             .key(function (d) { return d.Commodity; })
@@ -123,7 +238,7 @@ function importBar() {
                 };
             })
             .entries(data);
-        console.log(metrics);
+        // console.log(metrics);
 
         function topTen(data) {  //sorting to top 10 function
             data.sort(function (a, b) {
@@ -132,7 +247,7 @@ function importBar() {
             return data.slice(0, 10);
         }
 
-        console.log(topTen(metrics))
+        // console.log(topTen(metrics))
 
         // Define the div for the tooltip
         var div = d3.select("body").append("div")
@@ -188,67 +303,3 @@ function importBar() {
 }
 
 importBar();
-
-// Yearly Volume - Line Graph
-function lineGraph() {
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-        width = 800 - margin.left - margin.right,
-        height = 300 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3.select("#lineGraph")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function (data) {
-
-        // group the data: I want to draw one line per group
-        var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
-            .key(function (d) { return d.name; })
-            .entries(data);
-
-        // Add X axis --> it is a date format
-        var x_line = d3.scaleLinear()
-            .domain(d3.extent(data, function (d) { return d.year; }))
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x_line).ticks(5));
-
-        // Add Y axis
-        var y_line = d3.scaleLinear()
-            .domain([0, d3.max(data, function (d) { return +d.n; })])
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y_line));
-
-        // color palette
-        var res = sumstat.map(function (d) { return d.key }) // list of group names
-        var color = d3.scaleOrdinal()
-            .domain(res)
-            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
-
-        // Draw the line
-        svg.selectAll(".line")
-            .data(sumstat)
-            .enter()
-            .append("path")
-            .attr("fill", "none")
-            .attr("stroke", function (d) { return color(d.key) })
-            .attr("stroke-width", 1.5)
-            .attr("d", function (d) {
-                return d3.line()
-                    .x(function (d) { return x_line(d.year); })
-                    .y(function (d) { return y_line(+d.n); })
-                    (d.values)
-            })
-
-    })
-}
-
-lineGraph();
